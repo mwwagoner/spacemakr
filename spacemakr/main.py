fa38f43acdd59a4c30a03f2deea713ed01ea8c40
Michael Wagoner <mwwagoner@gmail.com>
2022-12-02 15:32:10 -0500

    Add the db initialization to an @app.before_first_request :

--- spacemakr/main.py
+++ spacemakr/main.py
@@ -7,9 +7,24 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///spacemakr.db"
 db = SQLAlchemy()
 db.init_app(app)
 
-with app.app_context():
+# with app.app_context():
+#     db.create_all()
+
+@app.before_first_request
+def initialize_database():
     db.create_all()
 
 @app.route('/')
 def hello():
     return "Hello"
+
+class Products(db.Model):
+    productID = db.Column(db.Integer, primary_key=True)
+    productName = db.Column(db.Text, nullable=False)
+
+class Orders(db.Model):
+    orderID = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date)
+    quantity = db.Column(db.Integer)
+    pricePerUnit = db.Column(db.Float)
+    test = db.Column
