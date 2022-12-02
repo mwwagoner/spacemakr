080986af4beed6853506d853bfe75c3568fd6810
Michael Wagoner <mwwagoner@gmail.com>
2022-12-02 08:49:47 -0500

    Collapse the models and app factory modules and remove the app factory

--- spacemakr/__init__.py
+++ spacemakr/__init__.py
@@ -1,21 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
-
 app = Flask(__name__)
-
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///spacemakr.db"
 
+db = SQLAlchemy()
 db.init_app(app)
-
-class Products(db.Model):
-    productID = db.Column(db.Integer, primary_key=True)
-    productName = db.Column(db.Text, nullable=False)
 
 with app.app_context():
     db.create_all()
 
-app.route('/hello')
+@app.route('/')
 def hello():
     return "Hello"
